f5fd2e85f3c7d5d6c4e417aa0028434736823fc2
VlSi <vova.home@gmail.com>
2021-01-10 12:07:37 +0100

    [Js+React] React base - props and events

--- 1. Js+React/ReactBase2/my-app/src/index.js
+++ 1. Js+React/ReactBase2/my-app/src/index.js
@@ -1,53 +1,62 @@
-// import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
-// const elem = React.createElement('h2', null, 'Hello World!!!')
-
-const Header = () => {
-  return <h2>Hello World</h2>;
-}
-
-const Field = () => {
-  const holder = "Enter here"
-  const styledField = {
-    width: '300px'
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 26
+    };
+    this.nextYear = this.nextYear.bind(this);
+    // this.nextYear = () => {
+    //   this.setState(state => ({
+    //     years: ++state.years
+    //   }));
+    // };
   }
-  return <input
-    style={styledField}
-    type="text"
-    placeholder={holder}
-    autoComplete=""
-    className="first"/>;
+
+  // state = {
+  //   years: 26
+  // };
+
+  // nextYear = () => {
+  //   this.setState(state => ({
+  //     years: ++state.years
+  //   }));
+  // };
+
+  nextYear() {
+    console.log(1);
+    this.setState(state => ({
+      years: ++state.years
+    }));
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    const {years} = this.state;
+    return (
+      <>
+        <button onClick={this.nextYear}>++</button>
+        <h1>My name is {name}, surname - {surname}, years - {years}</h1>
+        <a href={link}>My profile</a>
+      </>
+    );
+  }
 }
 
-const Btn = () => {
-  const text = "Log in"
-  const logged = true;
-  return <button>{logged ? "Enter" : text}</button>;
-}
-
-const App = () => {
+const All = () => {
   return (
-    <div>
-      <Header/>
-      <Field/>
-      <Btn/>
-    </div>
+    <>
+      <WhoAmI name="John" surname="Smith" link="google.com" />
+      <WhoAmI name="Mike" surname="Smith" link="facebox.com" />
+      <WhoAmI name="John" surname="Nice" link="vsote.com" />
+    </>
   );
 }
 
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <App/>,
+  <All />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
