50d30e8b4e8dec8fac23ad2a6510c3d1b8670656
VlSi <vova.home@gmail.com>
2021-02-18 21:55:04 +0100

    [Core+TS] End of section 3

--- 2. Complete_Guide_Core+React/Reactivities/Api/Startup.cs
+++ 2. Complete_Guide_Core+React/Reactivities/Api/Startup.cs
@@ -21,9 +21,19 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddDbContext<DataContext>(opt => 
+            services.AddDbContext<DataContext>(opt =>
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000");
+                });
             });
             services.AddControllers();
         }
@@ -41,6 +51,8 @@ namespace Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
